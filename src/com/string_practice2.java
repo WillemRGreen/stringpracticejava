35e5dc06808ebf1e360ca3af94b2fd33ec9c1b70
Will Green <willgreen@Wills-MacBook-Pro.local>
2021-05-18 13:41:23 -0500

    WG: added solution

--- src/com/string_practice2.java
+++ src/com/string_practice2.java
@@ -6,13 +6,13 @@ public class string_practice2 {
 
 	public static void main(String[] args) {
 
-		System.out.println(countWords(str1));
+		//System.out.println(countWords(str1));
+
+		// verticalString(str1);
+
+		// reverseVerticalString(str1);
 		
-		//verticalString(str1);
-		
-		//reverseVerticalString(str1);
-		
-		
+		System.out.println(findString(str1, "example"));
 
 	}
 
@@ -21,34 +21,53 @@ public class string_practice2 {
 		int wordCount = 0;
 
 		for (int i = 0; i < str.length(); i++) {
-				wordCount += str.substring(i, i+1).equals("\s") ? 1 : 0 ;
-			
+			wordCount += str.substring(i, i + 1).equals("\s") ? 1 : 0;
+
 		}
-		wordCount += wordCount > 0 ? 1 : 0; 
-		
+		wordCount += wordCount > 0 ? 1 : 0;
+
 		return wordCount;
 	}
-	
+
 	public static void verticalString(String str) {
+		String words = "";
 		
 		for(int i = 0; i < str.length(); i++) {
-			System.out.println(str.charAt(i));
+			if(str.substring(i, i+1).equals("\s")) {
+				words += "\n";
+			} else {
+				words += str.substring(i, i+1);
+			}
 		}
 		
+		System.out.println(words);
+
 	}
-	
+
 	public static void reverseVerticalString(String str) {
+
+		String words = "";
 		
-		for(int i = str.length()-1; i > -1; i--) {
-			System.out.println(str.charAt(i));
+		for(int i = 0; i < str.length(); i++) {
+			
+		}
+
+	}
+
+	public static boolean findString(String str1, String searchTerm) {
+		
+		String startTrigger = searchTerm.substring(0, 1);
+		
+		for(int i = 0; i < str1.length(); i++) {
+			if(str1.substring(i, i + 1).equals(startTrigger)) {
+				if(str1.substring(i, i + searchTerm.length()).equals(searchTerm)) {
+					return true;
+				}
+			}
 		}
 		
+		return false;
+
 	}
-	
-	public static void findString(String str) {
-		
-		
-		
-	}
-		
-	}
+
+}
